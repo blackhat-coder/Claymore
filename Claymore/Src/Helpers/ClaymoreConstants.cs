6657962fe33087801f00cfe6f5cf585a659b327b
Israel <abohisrael100@gmail.com>
2024-08-17 12:20:48 +0100

    Add constants file

---
+++ Claymore/Src/Helpers/ClaymoreConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claymore.Src.Helpers;
+
+public static class ClaymoreConstants
+{
+    public const string ResponseBody = "ResponseBody";
+    public const string ResponseHeader = "ResponseHeader";
+}
