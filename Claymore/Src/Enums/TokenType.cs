c3c5d4851165dc24de8f2b6a74298390249d31a2
abohisrael100@gmail.com <abohisrael100@gmail.com>
2024-08-27 02:25:16 +0100

    Add Token Types

---
+++ Claymore/Src/Enums/TokenType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Claymore.Src.Enums;
+
+public enum TokenType
+{
+    ReplacementToken,
+    BooleanToken,
+    StringToken,
+    NameToken,
+    NumberToken,
+    EmailToken
+}
