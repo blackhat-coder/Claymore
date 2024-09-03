40a9dd83d1fc7d7e173ece2516cd0c81f7aa6a66
abohisrael100@gmail.com <abohisrael100@gmail.com>
2024-09-03 16:38:10 +0100

    Add utility methods

---
+++ Claymore/Src/Utils/StringUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claymore.Src.Utils
+{
+    public static class StringUtil
+    {
+        public static string ReplaceUsingIndex(this string str, int startIndex, int length, string newStr)
+        {
+            if(startIndex < 0 || startIndex + length > str.Length)
+            {
+                throw new ArgumentNullException("Range is out of bounds");
+            }
+
+            StringBuilder sb = new StringBuilder(str);
+            sb.Remove(startIndex, length);
+            sb.Insert(startIndex, newStr);
+
+            return sb.ToString();
+        }
+    }
+}
