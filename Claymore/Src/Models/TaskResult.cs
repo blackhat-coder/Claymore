ecf3a58bb69cd48102b3d62de98a7f00e00a1575
abohisrael100@gmail.com <abohisrael100@gmail.com>
2025-08-17 20:52:25 +0100

    Add TaskResult Model

---
+++ Claymore/Src/Models/TaskResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claymore.Src.Models;
+
+/// <summary>
+/// A task represents a unit of work done by a worker. eg. Hitting a particular endpoint
+/// </summary>
+public class TaskResult
+{
+    public string Id { get; set; }
+    public string WorkerId { get; set; }
+    public string EndpointName { get; set; }
+    public int Order { get; set; }
+    public string? ResponseHeader { get; set; }
+    public string? ResponseBody { get; set; }
+    public bool Success { get; set; }
+    public int ElapsedTime { get; set; }
+}
