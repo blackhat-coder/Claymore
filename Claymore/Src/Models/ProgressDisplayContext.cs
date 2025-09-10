31d289138893d23694a0e41fd3e82cbe02736f1d
abohisrael100@gmail.com <abohisrael100@gmail.com>
2025-09-10 15:17:09 +0100

    Add models for displaying progress bars and returning result statistics

---
+++ Claymore/Src/Models/ProgressDisplayContext.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claymore.Src.Models;
+
+public class ProgressDisplayContext
+{
+    public ProgressContext progressContext { get; set; }
+    public ProgressTask progressTask { get; set; }
+    public int incrementBy { get; set; }
+}
---
+++ Claymore/Src/Models/TaskStatistic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claymore.Src.Models;
+
+public class TaskStatistic
+{
+    public string status { get; set; }
+    public string method { get; set; }
+    public string endpoint { get; set; }
+    public string name { get; set; }
+    public double workers { get; set; }
+    public double mean { get; set; }
+    public double max { get; set; }
+    public double min { get; set; }
+}
